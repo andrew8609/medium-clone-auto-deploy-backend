49fae431c9ff3edd4f9acef5da77ef2ca15aa199
ruslansobol <ruslansobol090@protonmail.com>
2022-03-09 00:23:51 -0800

    initial commit

--- config/auth.config.js
+++ config/auth.config.js
@@ -3,7 +3,6 @@ module.exports = {
     // jwtExpiration: 3600,         // 1 hour
     // jwtRefreshExpiration: 86400, // 24 hours
   
-    /* for test */
     jwtExpiration: 6000,          // 1 minute
     jwtRefreshExpiration: 12000,  // 2 minutes
     BACKEND_DOMAIN: "http://54.158.55.144:3008",
