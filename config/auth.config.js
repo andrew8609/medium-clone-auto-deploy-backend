050ee2f5594eb05267d8c293dad70d94650697b9
ruslansobol <ruslansobol090@protonmail.com>
2022-03-15 02:24:43 -0700

    fixed bugs

--- config/auth.config.js
+++ config/auth.config.js
@@ -5,7 +5,7 @@ module.exports = {
   
     jwtExpiration: 6000,          // 1 minute
     jwtRefreshExpiration: 12000,  // 2 minutes
-    BACKEND_DOMAIN: "http://happybirthdayhub.com:3000",
-    FRONTEND_DOMAIN: "http://happybirthdayhub"
+    BACKEND_DOMAIN: "http://happybirthdayhub.com",
+    FRONTEND_DOMAIN: "http://happybirthdayhub.com"
   };
   
