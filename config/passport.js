4c91c84f12574f820761a798996c93aa74ffde44
ruslansobol <ruslansobol090@protonmail.com>
2022-03-20 19:44:16 -0700

    oauth redirect uri err fixed

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,7 @@ passport.use(new GoogleStrategy(
   {
     clientID      : process.env.GOOGLE_CLIENT_ID,
     clientSecret  : process.env.GOOGLE_SECRET,
-    callbackURL   : `${config.BACKEND_DOMAIN}/auth/google/callback`,
+    callbackURL   : `${config.BACKEND_DOMAIN}/api/auth/google/callback`,
     passReqToCallback   : true
   }, function(request, accessToken, refreshToken, profile, done){
     
